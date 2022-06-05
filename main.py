2405734b6850e0e38fb920f1ca7712ad369cae0f
TYH71 <yuhoetan@gmail.com>
2022-06-05 16:51:15 +0800

    refactor: revert previous changes

--- main.py
+++ main.py
@@ -31,9 +31,9 @@ if __name__ == '__main__':
     # Sidebar - Receive Input Images
     sidebar = st.sidebar
     sidebar.header("Upload own Image")
-    file = sidebar.file_uploader("Upload Image", type=["jpg", "png", "jpeg"], accept_multiple_files=False, on_change=lambda _: print("Timestamp: {}".format(datetime.datetime.now())))
+    file = sidebar.file_uploader("Upload Image", type=["jpg", "png", "jpeg"], accept_multiple_files=False)
     sidebar.header("Demo Images")
-    demo = sidebar.button("Demo", on_click=lambda _: print("Timestamp: {}".format(datetime.datetime.now())))
+    demo = sidebar.button("Demo")
 
     # Load MDE Model
     model = load_model()
