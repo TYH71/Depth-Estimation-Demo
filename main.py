3836e8e563d845d6947a6ed123f4c5441ad16240
TYH71 <yuhoetan@gmail.com>
2022-06-06 23:11:05 +0800

    refactor: implement cache for model loading and image loading

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ import datetime
 HEIGHT = 256
 WIDTH = 256
 
-# @st.cache()
+@st.cache
 def load_model(path: str = "asset/MDE_256x256_50_16"):
     model = keras.models.load_model(path)
     return model
 
-# @st.cache()
+@st.cache
 def load_demo_image(path: str = "asset/*.jpg"):
     # Prepare Sample Image Data
     images = []
